60cd1d8863e508d325ff3c6db8ccec6d784e6b0f
Mohamed.Samir <Mohamed.elsayed@itworx.com>
2019-12-01 21:44:17 +0200

    - remove unused components - resolve connect get / error

--- VehicleDashboard.SPA/ClientApp/src/app/app.module.ts
+++ VehicleDashboard.SPA/ClientApp/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core'; 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router'; 
 import { AppComponent } from './app.component'; 
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CustomerVehicleDashboardComponent } from './customer-vehicle-dashboard/customer-vehicle-dashboard.component';
 import { CustomerVehicleDetailComponent } from './customer-vehicle-details/customer-vehicle-details.component';
 import { CustomerVehicleDashboardService } from './services/customer-vehicle-dashboard.service';
@@ -20,8 +17,6 @@ import { AlertifyService } from './Shared/alertify.service';
   declarations: [
     AppComponent,
     NavMenuComponent,
-    CounterComponent,
-    FetchDataComponent,
     CustomerVehicleDashboardComponent,
     CustomerVehicleDetailComponent
   ], 
@@ -30,16 +25,13 @@ import { AlertifyService } from './Shared/alertify.service';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
     PaginationModule.forRoot(),
     RouterModule.forRoot([
       {
         path: '', component: CustomerVehicleDashboardComponent },
       { path: 'customerVehicleHistory/:id/:vin/:regNo', component: CustomerVehicleDetailComponent },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: '**', redirectTo: '', pathMatch: 'full' }
-    ])
+    ]) 
   ],
   providers: [CustomerVehicleDashboardService, AlertifyService],
   bootstrap: [AppComponent]
